430f57dd398075aa3657b9d72a6786f6cd1789ea
priyank1303 <priyankbhalani90@gmail.com>
2021-10-01 16:45:40 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Quiz Teacher-side</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="heading">
+	<h1>Welcome to quiz master</h1>
+</div>
+
+<div class="wrap">
+	<div class="name">
+		<p>Ort</p>
+	</div>
+
+	<div class="name-input">
+		<input type="text" name="text" placeholder="enter your name">
+	</div>
+
+	<div class="question-input">
+		<input type="number" name="number" placeholder="How many questions do you want to ask?">
+	</div>
+</div>
+
+
+</body>
+</html>
